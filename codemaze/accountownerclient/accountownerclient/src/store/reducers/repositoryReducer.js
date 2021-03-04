c95ea8f7b891360828a08d4d53357e3e1b2f8194
mahedee <mahedee.hasan@gmail.com>
2021-03-04 23:24:41 +0600

    added some files

---
+++ codemaze/accountownerclient/accountownerclient/src/store/reducers/repositoryReducer.js
@@ -0,0 +1,44 @@
+const executeGetDataSuccess = (state, action) => {
+    return {
+        ...state,
+        data: action.data
+    }
+}
+
+const executePostDataSuccess = (state, action) => {
+    return {
+        ...state,
+        showSuccessModal: true
+    }
+}
+
+const executePutDataSuccess = (state, action) => {
+    return {
+        ...state,
+        showSuccessModal: true
+    }
+}
+
+const executeDeleteDataSuccess = (state, action) => {
+    return {
+        ...state,
+        showSuccessModal: true
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.GET_DATA_SUCCESS:
+            return executeGetDataSuccess(state, action);
+        case actionTypes.POST_DATA_SUCCESS:
+            return executePostDataSuccess(state, action);
+        case actionTypes.PUT_DATA_SUCCESS:
+            return executePutDataSuccess(state, action);
+        case actionTypes.DELETE_DATA_SUCCESS:
+            return executeDeleteDataSuccess(state, action);
+        default:
+            return state;
+    }
+}
+
+export default reducer;
