4f28ddeef727b5521972c6f54de204198984d2e6
Mahedee <mahedee.bd@asa-international.com>
2021-03-14 09:46:08 +0600

    dfd

--- Practice01/practice/src/index.js
+++ Practice01/practice/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -236,6 +236,47 @@ function NumberList(props){
 }
 
 const numbers = [1, 2, 3, 4, 5];
+
+
+class NameForm extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {value: 'Coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event){
+    alert('Your favorite fruit is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Essay: 
+          {/* <input type="text" value={this.state.value} onChange={this.handleChange} /> */}
+          {/* <textarea value={this.state.value} onChange={this.handleChange} /> */}
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">GrapeFruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit"></input>
+      </form>
+    );
+  }
+}
+
+
 ReactDOM.render(
   // <h1>Hello, world!</h1>,
   //element,
@@ -245,7 +286,8 @@ ReactDOM.render(
   // <Greeting isLoggedIn={false} />,
   // <LoginControl></LoginControl>,
   // <Page></Page>,
-  <NumberList numbers={numbers}></NumberList>,
+  // <NumberList numbers={numbers}></NumberList>,
+  <NameForm></NameForm>,
   document.getElementById('root')
 );
 
