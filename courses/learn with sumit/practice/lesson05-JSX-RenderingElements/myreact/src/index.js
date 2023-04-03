49c1aa74ef3b04abe3905c1f9aa090a269bc5eb9
mahedee <mahedee.hasan@gmail.com>
2023-04-02 22:30:53 -0300

    ...

--- courses/learn with sumit/practice/lesson05-JSX-RenderingElements/myreact/src/index.js
+++ courses/learn with sumit/practice/lesson05-JSX-RenderingElements/myreact/src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+// Exmaple : 1
+// const index = 0;
+// const element = (
+//   <h1 className='heading' tabIndex={index}>
+//     <span className='text'>Hello {new Date().toLocaleTimeString()}</span>
+//   </h1>
+// );
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
 
 const index = 0;
-const element = (
-    <h1 className="heading" tabIndex={index}>
-        <span className="text">Hello world</span>
-        {/* <img src=""/> */}
-    </h1>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(element, document.getElementById('root'));
+setInterval(()=>{
+  const element = (
+  <h1 className='heading' tabIndex={index}>
+    <span className='text'>Hello {new Date().toLocaleTimeString()}</span>
+  </h1>
+);
+root.render(element);
+
+}, 1000);
