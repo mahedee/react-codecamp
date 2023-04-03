19a0f02cfee4e7b846590ee2f82c2cd93462c170
mahedee <mahedee.hasan@gmail.com>
2023-04-03 19:04:17 -0300

    ....

--- courses/learn with sumit/practice/lesson06-Component-Props/myreact/src/index.js
+++ courses/learn with sumit/practice/lesson06-Component-Props/myreact/src/index.js
@@ -1,9 +1,11 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import React from 'react';
 
 
 
+   
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //  function Clock() {
@@ -17,14 +19,28 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
   // This one will work 
   //root.render(Clock()); 
 
-  function Clock({locale}) {
-    return (
+
+  // Example 2:
+  // function Clock({locale}) {
+  //   return (
+  //     <h1 className='heading'>
+  //       <span className='text'>Hello {new Date().toLocaleTimeString(locale)}</span>
+  //     </h1>
+  //   )
+  //  }
+
+
+  // root.render(<Clock locale="bn-B D"/>)
+
+  // Exaple 3:
+  class Clock extends React.Component{
+    render(){
+      return(
       <h1 className='heading'>
-        <span className='text'>Hello {new Date().toLocaleTimeString(locale)}</span>
+        <span className='text'>Hello - {this.props.children} {new Date().toLocaleTimeString(this.props.locale)}</span>
       </h1>
-    )
-   }
+      );
+    }
+  }
 
-   
-
-  root.render(<Clock locale="bn-BD"/>)
+  root.render(<Clock locale="bn-BD">test</Clock>);
