37207d58d3520a8848aabfaf81512fe5b96b89e3
mahedee <mahedee.hasan@gmail.com>
2023-05-13 19:56:15 -0300

    ....

---
+++ courses/learn with sumit/practice/lesson25-styling/my-react/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+//import './App.css';
+
+import './assets/css/global.css'
+import Logo from './components/Logo';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Learn with Sumit</h1>
+      <Logo></Logo>
+    </div>
+  );
+}
+
+export default App;
--- courses/learn with sumit/readme/readme.txt
+++ courses/learn with sumit/readme/readme.txt
@@ -2,7 +2,7 @@ References:
 
 https://www.youtube.com/playlist?list=PLHiZ4m8vCp9M6HVQv7a36cp8LKzyHIePr
 
-Complete upto #23
+Complete upto #24
 
 //javascript compiler
 https://babeljs.io/ 
