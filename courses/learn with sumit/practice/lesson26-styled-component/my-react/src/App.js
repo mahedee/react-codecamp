74328e82849cbd4b2fe75220070aad559721854c
mahedee <mahedee.hasan@gmail.com>
2023-05-13 21:58:40 -0300

    ...

---
+++ courses/learn with sumit/practice/lesson26-styled-component/my-react/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Tutorial from './components/Tutorial';
+import GlobalStyle from './components/styles/Global.styles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle></GlobalStyle>
+      <Tutorial></Tutorial>
+    </>
+  );
+}
+
+export default App;
--- courses/learn with sumit/readme/readme.txt
+++ courses/learn with sumit/readme/readme.txt
@@ -2,7 +2,7 @@ References:
 
 https://www.youtube.com/playlist?list=PLHiZ4m8vCp9M6HVQv7a36cp8LKzyHIePr
 
-Complete upto #24
+Complete upto #26
 
 //javascript compiler
 https://babeljs.io/ 
